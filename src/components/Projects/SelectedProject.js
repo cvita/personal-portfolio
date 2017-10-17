d615ed83a4f92c10d817b3e63346b659c859dddc
Chris Vita <chris.vita@gmail.com>
2017-10-17 15:25:04 -0700

    Add to `<SelectedProject />` layout

--- src/components/Projects/SelectedProject.js
+++ src/components/Projects/SelectedProject.js
@@ -1,10 +1,88 @@
 import React, { Component } from 'react';
 import initialState from '../../redux/initialState';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button, ListGroup, ListGroupItem } from 'reactstrap';
+import YouTubePlayer from '../helper/YouTubePlayer';
+import './SelectedProject.css';
 
+
+const TechStack = props => (
+  <ListGroup>
+    {props.list.split(', ').map(tech => {
+      return (<ListGroupItem className='subtitle select' key={tech}>
+        <i className="fa fa-check-circle-o" aria-hidden="true"></i> {tech}
+      </ListGroupItem>);
+    })}
+  </ListGroup>
+);
+
+const SelectedProjectLayout = props => {
+  const {
+    title,
+    titlePretty,
+    images,
+    short_description,
+    long_description,
+    github, demo_url,
+    tech_stack,
+    travis,
+    video
+    } = props;
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h1 className='title'>
+            {titlePretty}
+            {travis &&
+              <span>
+                {' '}<img src={`https://travis-ci.org/VitaC123/${title}.svg?branch=master`} alt='travis-ci' />
+              </span>}
+          </h1>
+          <p className='subtitle lead'>{short_description}</p>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md='8' xs='12'>
+          <div className='featuredMedia'>
+            {video ?
+              <YouTubePlayer videoId={video} title={title} /> :
+              <img src={images.large.source_url} alt={title} />}
+          </div>
+
+          <div className='viewButtonContainer'>
+            <h3 className='sectionHeading selected view'>View</h3>
+            <a href={github}><Button className='viewButton' color='link'>
+              <i className="fa fa-github" aria-hidden="true"></i> Source</Button>
+            </a>
+            <a href={demo_url}><Button className='viewButton' color='link'>
+              <i className="fa fa-desktop" aria-hidden="true"></i> Demo</Button>
+            </a>
+          </div>
+        </Col>
+
+        <Col md='4' xs='12'>
+          {/* intentionally empty */}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md='8' xs='12'>
+          <h3 className='sectionHeading selected'>Project background</h3>
+          <p className='bodyText selected' dangerouslySetInnerHTML={{ __html: long_description }} />
+        </Col>
+        <Col md='4' xs='12'>
+          <h3 className='sectionHeading selected'>Technologies</h3>
+          <TechStack list={tech_stack} />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 class SelectedProject extends Component {
   componentDidMount() {
+    window.scrollTo(0, 0);
     const url = window.location.pathname.toString();
     const titleInUrl = url.slice(url.indexOf('/projects/') + 10);
     const { projects } = this.props;
@@ -15,31 +93,10 @@ class SelectedProject extends Component {
     this.props.makeSelectedProject(initialState.selectedProject);
   }
   render() {
-    const { title, titlePretty, images, short_description, long_description, tech_stack } = this.props.selectedProject;
-
     return (
-      <Container>
-        <Row>
-          <Col>
-            <h1>{titlePretty}</h1>
-            <h5>{short_description}</h5>
-
-            {images && images.medium && (
-              <img src={images.medium.source_url} alt={title} />)}
-
-          </Col>
-        </Row>
-
-        <Row>
-          <Col md='8' sm='12'>
-            <p dangerouslySetInnerHTML={{ __html: long_description }} />
-          </Col>
-          <Col md='4' sm='12'>
-            <ul>
-              <li>{tech_stack}</li>
-            </ul>
-          </Col>
-        </Row>
+      <Container className='selectedProject'>
+        {typeof this.props.selectedProject.title === 'string' &&
+          <SelectedProjectLayout {...this.props.selectedProject} />}
       </Container>
     );
   }
