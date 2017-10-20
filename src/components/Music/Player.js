c0cf950c242599fbac380ae68e01d573797eb2de
Chris Vita <chris.vita@gmail.com>
2017-10-20 10:38:15 -0700

    Add notification to user
    Lifecycle methods to ensure component is visible on relevant actions.

--- src/components/Music/Player.js
+++ src/components/Music/Player.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Collapse, Button } from 'reactstrap';
+import { Row, Col, Collapse, Alert, Button } from 'reactstrap';
 import './Player.css';
 
 
@@ -7,10 +7,25 @@ class Player extends Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
-    this.state = { collapse: true };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.state = {
+      collapse: false,
+      visible: true
+    };
+  }
+  componentDidMount() {
+    this.setState({ collapse: true });
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.albumId !== nextProps.albumId) {
+      this.setState({ collapse: true });
+    }
   }
   toggle() {
     this.setState({ collapse: !this.state.collapse });
+  }
+  onDismiss() {
+    this.setState({ visible: false });
   }
   render() {
     const { albumId, size, tracklist, artwork } = this.props;
@@ -19,21 +34,33 @@ class Player extends Component {
       <i className='fa fa-angle-double-up' aria-hidden='true' />;
     return (
       <div className='player'>
-        <Row>
-          <Col sm='8' xs='12'>
-            <Collapse isOpen={this.state.collapse}>
-              <iframe
-                className='bandcampIframe'
-                title={albumId}
-                src={`https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=${size}/bgcol=ffffff/linkcol=0687f5/tracklist=${tracklist}/artwork=${artwork}/transparent=true/`}
-                seamless>
-              </iframe>
-            </Collapse>
-          </Col>
-          <Col sm='4' xs='12'>
-            <Button className='showHideButton' onClick={this.toggle} color='secondary' >{showHide}</Button>
-          </Col>
-        </Row>
+        <div className='container-fluid'>
+          <Row>
+
+            <Col sm='8' xs='12'>
+              <Collapse isOpen={this.state.collapse}>
+                <iframe
+                  className='bandcampIframe'
+                  autoPlay
+                  title={albumId}
+                  src={`https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=${size}/bgcol=ffffff/linkcol=0687f5/tracklist=${tracklist}/artwork=${artwork}/transparent=true/`}
+                  seamless>
+                </iframe>
+              </Collapse>
+            </Col>
+
+            <Col sm='4' xs='12'>
+              <div className='playerRight'>
+                <Alert className='infoAlert' isOpen={this.state.visible} color='info'>
+                  <p>This player will remain available while you browse.</p>
+                  <Button onClick={this.onDismiss} color='primary'>Got it!</Button>
+                </Alert>
+                <Button className='showHideButton' onClick={this.toggle} color='secondary' >{showHide}</Button>
+              </div>
+            </Col>
+
+          </Row>
+        </div>
       </div>
     );
   }
