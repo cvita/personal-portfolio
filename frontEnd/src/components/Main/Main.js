befeb57084016d73b36a1086612dc34b7f089095
Chris Vita <chris.vita@gmail.com>
2017-10-27 13:29:35 -0700

    Add layout revision to `<Projects />` and subcomponents

--- frontEnd/src/components/Main/Main.js
+++ frontEnd/src/components/Main/Main.js
@@ -12,39 +12,60 @@ import Footer from './Footer';
 import './Main.css';
 
 
-const Main = props => (
-  <div className='mainBody'>
+const Main = props => {
+  const {
+    siteText,
+    projects,
+    selectedProject,
+    routing,
+    fetchSiteText,
+    fetchProjects,
+    makeSelectedProject,
+    } = props;
 
-    <div className='mainContent'>
-      <header>
-        <Navigation {...props.routing.location} />
-      </header>
-      <main>
-        <Switch>
-          <Route path='/' exact={true} render={() => (
-            <div>
-              <Home {...props.siteText} fetchSiteText={props.fetchSiteText} />
-              <Projects {...props} />
-            </div>
-          )}
-          />
-          <Route path='/projects' render={() => <Projects {...props} />} />
-          <Route path='/music' render={() => <Music {...props} />} />
-          <Redirect from='/personal-portfolio' to='/' />
-          <Route component={NoMatch404} />
-        </Switch>
-      </main>
+  const projectsComponent = (
+    <Projects
+      projects={projects}
+      selectedProject={selectedProject}
+      fetchProjects={fetchProjects}
+      makeSelectedProject={makeSelectedProject}
+      routing={routing}
+    />
+  );
+  return (
+    <div className='mainBody'>
+
+      <div className='mainContent'>
+        <header>
+          <Navigation {...routing.location} />
+        </header>
+        <main>
+          <Switch>
+            <Route path='/' exact={true} render={() => (
+              <div>
+                <Home{...siteText} fetchSiteText={fetchSiteText} />
+                {projectsComponent}
+              </div>
+            )}
+            />
+            <Route path='/projects' render={() => projectsComponent} />
+            <Route path='/music' render={() => <Music {...props} />} />
+            <Redirect from='/personal-portfolio' to='/' />
+            <Route component={NoMatch404} />
+          </Switch>
+        </main>
+      </div>
+
+      {props.selectedMusic &&
+        <Player {...props.selectedMusic} />}
+
+      <footer className='mainFooter'>
+        <Footer />
+      </footer>
+
     </div>
-
-    {props.selectedMusic &&
-      <Player {...props.selectedMusic} />}
-
-    <footer className='mainFooter'>
-      <Footer />
-    </footer>
-
-  </div>
-);
+  );
+}
 
 
 export default Main;
